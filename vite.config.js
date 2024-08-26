eb8f08cd73163c182d3b6ce2a379cf0371c70d22
Azimnarimanov2003 <161734407+Azimnarimanov2003@users.noreply.github.com>
2024-08-26 18:21:15 +0500

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
+      
       external: [
         'i18next-browser-languagedetector',
         // boshqa tashqi modullar agar mavjud bo'lsa
